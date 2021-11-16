631f91845ea4a77ed7be23e7cbf8b9d4d8e7114e
Denesterio <mrdnsong@gmail.com>
2021-11-16 11:01:08 +0300

    creating with stub text

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,39 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container bg-light shadow rounded h-100 p-4">
+    <div class="text-center p-2">выбрать размер текста:<br />маленький | средний | большой</div>
+    <div class="text-box bg-dark text-light rounded">
+      <div v-if="text.length > 0" class="p-3">{{ text }}</div>
+    </div>
+    <p>{{ tLength }}</p>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      text: '«Зеленые жнецы» (The Green Reapers) — работа французского видеохудожника Томаса Бланшара. Четыре месяца он, вооружившись камерой с разрешением 8K, снимал мир насекомых и поедающих их растений. Венерины мухоловки, росянки и непентесы (напоминающие кувшины ловушки) запечатлены за убийством и поглощением животных. И выглядит это завораживающе. Автор начал с саспенса и закончил слэшером — настоящий сплаттерпанк.'
+    }
+  },
+
+  computed: {
+    tLength() {
+      return this.text.length;
+    }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.text-box {
+  min-height: 200px;
+  line-height: 1.5;
+  padding: 1rem;
+  font-size: 1.2rem;
+  width: 100%;
+  max-width: 900px;
+  margin: 0 auto;
 }
 </style>
